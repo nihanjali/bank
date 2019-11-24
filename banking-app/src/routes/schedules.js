fa7186d48ad834d5f9094961178029aa0214450d
farhakauser-1 <farha.kauser1@gmail.com>
2019-11-23 21:45:58 -0800

    Schedule

--- banking-app/src/routes/schedules.js
+++ banking-app/src/routes/schedules.js
@@ -10,7 +10,7 @@ router.post("/transfer", (req, res) => {
     if (error)
         res.status(STATUS_CODE.BAD_REQUEST).send(error.details[0].message);
 
-    let sql = ``;
+    let sql = `CALL Schedule_Transfer_put(${req.body.user_id}, ${req.body.from_account_number}, ${req.body.to_account_number}, ${req.body.trans_amount}, ${req.body.transfer_interval_days});`;
     pool.query(sql, (err, result) => {
         if (err) {
             console.log(err);
@@ -27,7 +27,7 @@ router.post("/paybill", (req, res) => {
     if (error)
         res.status(STATUS_CODE.BAD_REQUEST).send(error.details[0].message);
 
-    let sql = ``;
+    let sql = `CALL Schedule_Bill_Payment_put(${req.body.user_id}, ${req.body.from_account_number}, '${req.body.bill_payee}', ${req.body.trans_amount}, ${req.body.transfer_interval_days});`;
     pool.query(sql, (err, result) => {
         if (err) {
             console.log(err);
@@ -44,7 +44,7 @@ router.get("/:user_id/:account_number", (req, res) => {
     if (error)
         res.status(STATUS_CODE.BAD_REQUEST).send(error.details[0].message);
 
-    let sql = ``;
+    let sql = `SELECT * FROM schedules WHERE user_id = ${req.params.user_id} AND (from_account_number = ${req.params.account_number} OR to_account_number = ${req.params.account_number});`;
     pool.query(sql, (err, result) => {
         if (err) {
             console.log(err);
@@ -60,7 +60,7 @@ router.get("/:user_id/:account_number/:trans_type", (req, res) => {
     if (error)
         res.status(STATUS_CODE.BAD_REQUEST).send(error.details[0].message);
 
-    let sql = ``;
+    let sql = `SELECT * FROM schedules WHERE user_id = ${req.params.user_id} AND (from_account_number = ${req.params.account_number} OR to_account_number = ${req.params.account_number}) AND trans_type LIKE '${req.params.trans_type}';`;
     pool.query(sql, (err, result) => {
         if (err) {
             console.log(err);
